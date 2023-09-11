9457f33f9fd45befc52270ecc226c50503510747
Sherly Jasmine <sherlyjasmine97@gmail.com>
2023-09-11 09:07:40 +0700

    ditambahin pemahaman slicing

--- gitcircular_array_rotation.py
+++ gitcircular_array_rotation.py
@@ -1,12 +1,17 @@
 # Problem = melakukan operasi rotasi selama x kali dan tentukan
 # nilai elemen dari posisi yang ditentukan
 
-def circularArrayRotation(a, k, queries):
-    k=k%len(a)
-    temporaryarray=a[-k:] + a[:-k]
-    for i in range(len(queries)):
-        queries[i]=temporaryarray[queries[i]]
-    return queries
+# def circularArrayRotation(a, k, queries):
+#     k=k%len(a)
+#     temporaryarray=a[-k:] + a[:-k]
+#     for i in range(len(queries)):
+#         queries[i]=temporaryarray[queries[i]]
+#     return queries
 
-print(circularArrayRotation([1,2,3], 2,[0,1,2])) #2,3,1
+# print(circularArrayRotation([1,2,3], 2,[0,1,2])) #2,3,1
 
+a=[1,2,3]
+k=2
+slicing=a[-k:]
+slicing2=a[:-k]
+print(slicing,slicing2)
