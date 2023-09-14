ea00ed7efa3e5b1e8113245e984d31ee7102c6ba
Sherly Jasmine <sherlyjasmine97@gmail.com>
2023-09-14 15:26:43 +0700

    makin bingung

--- gitlatihanmembuatapi_circulaarrayrotation.py
+++ gitlatihanmembuatapi_circulaarrayrotation.py
@@ -4,29 +4,49 @@ import base64
 
 app= Flask(__name__)
 
+#error handler
+@app.errorhandler(400)
+def badRequest():
+    return {'ERROR 400 - Bad Request' : 'The request was malformed or invalid'},400
+
+@app.errorhandler(401)
+def unauthorized():
+    return {'ERROR 401 - Unauthorized' : 'You are not authorized to access this resource'},401
+
 @app.route('/circular-array-rotation', methods=['GET','POST'])
 def searchCircularArrayRotation():
+    #authentification
     username = 'pengabdisetan'
     password = 'pengabdisetan666'
     strauth = f'{username}:{password}'
     encryption = strauth.encode('ascii')
     base64_auth_str = f'Basic {str(base64.b64encode(encryption))[2:-1]}'
     header = request.headers.get('Authorization')
+    #data validation error 401
     if header!= base64_auth_str :
-       return 'ERROR', 401
+       return unauthorized(),401
 
-
+    #body parameter
     data = request.json
     a=data['a'] #[1,2,3]
     k=data['k'] #2
     queries = data['queries'] #[0,1,2]
+    
+    #data validation error 404
+    if len(a)==0 or k==0 or len(queries)==0:
+        return badRequest(),400
+    # required_data = ['a', 'k', 'queries']
+    # for data_key in required_data :
+    #     data_keyfound=False
+    #     for key in data.keys() :
+    #         if data_key == key:
+    #             data_keyfound=True
+    #             break
+    #         else :
+    #             return badRequest(),400
 
     result = circularArrayRotation(a,k,queries)
     return jsonify({'result': result})
-
-@app.errorhandler(400)
-def badRequest(error):
-    return {'message': 'Bad Request: The request was malformed or invalid.'},404
 
 if __name__ == '__main__':
     app.run(debug=True)
@@ -49,4 +69,3 @@ if __name__ == '__main__':
 #     queries = queries1.split(',')
 #     for i in queries:
 #         i=int(i)
-    
